a0853652600f423bf95848cc2abfbce47bc51a65
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2022-05-13 05:25:04 +0000

    Yes Lets gooo

--- code1.py
+++ code1.py
@@ -3,102 +3,110 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 # Определяем переменную величину
-frames = 30
-seconds_in_year = 30 * 24 * 60 * 60
-years = 1.5
-t = np.linspace(0, years*seconds_in_year, frames)
+frames = 100
+seconds_in_cycle = 60
+cycles = 50
+t = np.linspace(0, cycles * seconds_in_cycle, frames)
 
 # Определяем функцию для системы диф. уравнений
 def move_func(s, t):
-    (x1, v_x1, y1, v_y1,
-     x2, v_x2, y2, v_y2) = s
+    (x, v_x, y, v_y) = s
 
-    dxdt1 = v_x1
-    dv_xdt1 = - G * m * x1 / (x1**2 + y1**2)**1.5
-    dydt1 = v_y1
-    dv_ydt1 = - G * m * y1 / (x1**2 + y1**2)**1.5
+    dxdt = v_x
+    dv_xdt = - G * earthM * x / (x**2 + y**2)**1.5
+    dydt = v_y
+    dv_ydt = - G * earthM * y / (x**2 + y**2)**1.5
 
-    dxdt2 = v_x2
-    dv_xdt2 = - G * m * x2 / (x2**2 + y2**2)**1.5
-    dydt2 = v_y2
-    dv_ydt2 = - G * m * y2 / (x2**2 + y2**2)**1.5
-
-    return (dxdt1, dv_xdt1, dydt1, dv_ydt1,
-            dxdt2, dv_xdt2, dydt2, dv_ydt2)
+    return (dxdt, dv_xdt, dydt, dv_ydt)
     
 # Определяем начальные значения и параметры
-    
+satH = 408 * 10**3 # 408 км высотп МКС
+satV = 7.66 * 10**3 # скорость МКС
+satM = 420 * 10**3 # масса МКС
+
+earthR = 6371 * 10**3
+earthM = 5.9742 * 10**24
+
 G = 6.67 * 10**(-11)
-m = 1.98 * 10**(30)
 
-x10 = -0.387 * 149 * 10**9
-v_x10 = 0
-y10 = 0
-v_y10 = 41000
+x0 = (earthR + satH)
+v_x0 = 0
+y0 = 0.0
+v_y0 = satV
 
-x20 = 0
-v_x20 = -32020
-y20 = 0.723332 * 149 * 10**9
-v_y20 = 0
-
-s0 = (x10, v_x10, y10, v_y10,
-      x20, v_x20, y20, v_y20)
+s0 = (x0, v_x0, y0, v_y0)
 
 sol = odeint(move_func, s0, t)
-#Цикл над sol 
+
+post_sol = [[],[],[],[]]
+
+for i in range(frames):
+	x = sol[i, 0]
+	y = sol[i, 2]
+	h = y - earthR
+	d = np.sqrt( x**2 + h**2 )
+
+	if h > 0:
+		a = np.arccos(x / d)
+		u = np.cos(a)
+		v = np.sin(a)
+		adeg = np.degrees(a)
+		post_sol[0].append(adeg if (adeg < 90) else (180 - adeg))
+		post_sol[1].append(u)
+		post_sol[2].append(v)
+	else:
+		post_sol[0].append(0)
+		post_sol[1].append(0)
+		post_sol[2].append(0)
+	post_sol[3].append(h)
+		
+
 # Решаем систему диф. уравнений
 def solve_func(i, key):
     if key == 'point':
-        x1 = sol[i, 0]
-        y1 = sol[i, 2]
-        x2 = sol[i, 4]
-        y2 = sol[i, 6]
+        x = sol[i, 0]
+        y = sol[i, 2]
 
     else:
-        x1 = sol[:i, 0]
-        y1 = sol[:i, 2]
-        x2 = sol[:i, 4]
-        y2 = sol[:i, 6]
+        x = sol[:i, 0]
+        y = sol[:i, 2]
 
-    return ((x1, y1), (x2, y2))
+    return (x, y)
 
 # Строим решение в виде графика и анимируем
 fig, ax = plt.subplots()
+ball1, = plt.plot([], [], 'o', color='r', ms=5)
+horizont, = plt.plot([-10**8, 10**8], [earthR, earthR], '--', color='g')
+quiver = ax.quiver(0, earthR, 0, 0, scale=4, width=0.01)
+circle = plt.Circle((0.0, 0.0), earthR, color="#8080ff")
+ax.add_patch(circle)
 
-ball1, = plt.plot([], [], 'o', color='r', ms=15)
+def update_quiver(i):
+	u, v, h = post_sol[1][i], post_sol[2][i], post_sol[3][i]
 
-def get_arrow(theta):
-    x = 0
-    y = 10e4
-    u = np.sin(theta)
-    v = np.cos(theta)
-    return x, y, u, v
+	quiver.set_UVC(u, v)
 
-quiver = ax.quiver(*get_arrow(100), scale=4, width=0.001)
-
-def update(theta):
-    global quiver
-    quiver.remove()
-    quiver = ax.quiver(*get_arrow(theta), scale=4, width=0.001)
-
-plt.plot([0], [0], 'o', color='orange', ms=40)
+	return quiver,
 
 def animate(i):
-    ball1.set_data(solve_func(i, 'point')[0])
+	ball1.set_data(solve_func(i, 'point'))
+	update_quiver(i)
 
 
 ani = FuncAnimation(fig,
                     animate,
-                    frames=30,
-                    interval=30)
-
-ani1 = FuncAnimation(fig,
-                    update,
-                    frames=30,
-                    interval=30)
+                    frames=frames,
+					interval=30)
 
 plt.axis('equal')
-edge = 10e10
+edge = (earthR + satH) * 1.1
 ax.set_xlim(-edge, edge)
-ax.set_ylim(-edge, edge)
+ax.set_ylim(earthR , edge + earthR)
+#ani.save("arrow.gif", dpi=200)
+plt.show()
+
+ax.set_xlim(1000, 2000)
+atg, = plt.plot(t, post_sol[0], '-', color='orange')
+plt.savefig("fig.png", dpi=200)
+ani.save('1.gif')
 plt.show()
